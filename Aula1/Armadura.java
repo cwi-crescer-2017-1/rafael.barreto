e34ac5b8ff4a9ddd76f08997535856d536707006
Rafael Barreto <rsmennabarreto@gmail.com>
2017-04-13 23:39:35 -0300

    feat(armadura):adicionado methodo getCategoria

--- Aula1/Armadura.java
+++ Aula1/Armadura.java
@@ -1,9 +1,13 @@
 public class Armadura{
     private String constelacao;
-    private Categoria categoria = Categoria.BRONZE;
+    private Categoria categoria;
     
     public Armadura(String constelacao, Categoria categoria ){
         this.constelacao=constelacao;   
         this.categoria = categoria;
-    }    
+    }   
+    
+    public Categoria getCategoria(){   
+        return this.categoria;
+    }
 }
