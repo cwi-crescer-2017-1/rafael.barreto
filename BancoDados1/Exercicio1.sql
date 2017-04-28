7e71f27553fd95ac43b0b67252965308c3014841
rafaelbarreto <rafael.barreto@cwi.com.br>
2017-04-28 17:44:32 -0300

    doc(AulaBD): arquivos com dados da aula.

--- BancoDados1/Exercicio1.sql
+++ BancoDados1/Exercicio1.sql
@@ -1,3 +1,4 @@
+
 --exercicio 1
 
 select *
@@ -8,24 +9,24 @@ from cidade ,
 --exercicio 2
 truncate table CidadeAux -- > limpa tabela	
 
-insert into cidadeAux
-select *
+insert into cidadeAux (IDCidade,nome,uf) --> boa pratica colocar nome das colunas a serem copiadas
+select (IDCidade,nome,uf)
 from cidade
-where uf = 'RS'
+where uf = 'RS';
 
--- insere dados na tabela auxiliar onde o campo uf tiver o valor RS
+--> insere dados na tabela auxiliar onde o campo uf tiver o valor RS
 
 --exercicio 3
 
 create table Produto(
-	IDProduto int identity not null,	
-	Nome varchar(15) not null,
-	descricao varchar(40),
-	dataCriacao varchar (10),
+	IDProduto       int identity not null,	
+	Nome            varchar(15) not null,
+	descricao       varchar(40),
+	dataCriacao     varchar (10),
 	localArmazenado varchar(20),
-	Quantidade int,
-	Preço float,
-	constraint PK_IDProdutos primary key(IDProduto) 
+	Quantidade      decimal(7,3), --> tem sete posicoes com 3 decimais ex: 9999.000
+	Preço			decimal(9,2),
+	 constraint PK_IDProdutos primary key(IDProduto) 
 )
 
 --exercicio 4
@@ -37,5 +38,12 @@ values('produto1');
 insert into produto (nome)
 values('produto2');
 
-select * from produto
+select localArmazenado as estoque  
+from produto
 
+select getdate(); --> puxa a data e hora atual do sistema(obs: data e hora do servidor )
+
+select * from cidade
+order by(nome);
+
+/*  
