2429e986c59d95eef8cb14f4843a2d4e88220ecd
rafaelbarreto <rafael.barreto@cwi.com.br>
2017-05-14 12:51:07 -0300

    feat(exercicio02Modulo04): adicionar  funcao easterEgg

--- modulo 04/aula02/exercicio/exercicio02.js
+++ modulo 04/aula02/exercicio/exercicio02.js
@@ -60,8 +60,8 @@ function procurarPorNome(series,nome){
 let nome1="";
 
 	for(let i in series){
-		nome1 = series[i].elenco.find((elemento,id,array) => elemento === nome);
-		if(nome1 == nome) return true;
+		nome1 = series[i].elenco.filter(elemento => elemento.includes(nome));
+		if(nome1 > nome) return true;
 	}
 
 	return false;	
@@ -69,7 +69,7 @@ let nome1="";
 }
 
 console.log('exercicio 04')	
-console.log( procurarPorNome(series, "Winona Ryder"));
+console.log( procurarPorNome(series, "Winona"));
 
 //------------- exercicio 05 -------------------------------
 
@@ -103,13 +103,13 @@ function queroGenero(series,genero){
 
 function queroTitulo(series,titulo){
 
-	return series.filter(serie => serie.titulo.indexOf(titulo)>=0)				 
+	return series.filter(serie => serie.titulo.toUpperCase().indexOf(titulo.toUpperCase())>=0)				 
 				 .map(series => series.titulo);
 }
 
 console.log('exercicio 06');
 console.log(queroGenero(series,"Caos"));
-console.log(queroTitulo(series,"The"));
+console.log(queroTitulo(series,"the"));
 
 //------------- exercicio 07 -------------------------------
 
@@ -132,8 +132,8 @@ function creditosIlluminatis(serie){
 	let diretores = serie.diretor;
 	let elenco = serie.elenco;
 
-	// ordenaSobrenome(diretores);
-	// ordenaSobrenome(elenco);
+	diretores.sort(ordenaSobrenome);
+	elenco.sort(ordenaSobrenome);
 	
 	console.log('-----------------------DIRETORES-------------------------')
 	diretores.forEach(diretor => console.log((diretor)))
@@ -145,3 +145,65 @@ creditosIlluminatis(series[0]);
 
 
 //------------- exerccio 08 -----------------------------------
+
+// Serie Illuminati
+
+// Escondemos um pequeno easter egg neste exercicio!
+// Para que ele seja descoberto, será necessário algumas informações; Quando abreviamos um nome colocamos a primeira letra do nome seguida de um ponto final
+// Exemplo:
+// Bernardo Bosak Rezende -> Bernardo B. Rezende
+// Augusto Schiller Wartchow -> Augusto S. Wartchow
+// Essa é a informação básica! Construa uma função que identificará aquela série que tem TODOS do elenco com nomes abreviados.
+// Dica: Construa uma função separada para identificar se aquela String tem a abreviação;
+// Show de bola, estamos quase lá!
+// Uma vez achada a série, vamos modificar um pouquinho a implementação. Coloque todas as palavras abreviadas 
+// (de preferência sem os pontos finais) em uma string que será retornada ao final do método.
+// Forme uma hashtag com a palavra! #PALAVRA
+
+// series[8].elenco[0].split(" ",2).slice(1) ----> separa a letra com .
+
+//series[8].elenco.forEach(elenco => console.log(elenco.split(" ",2).slice(1)))// retorna todas letras do easter egg
+
+
+ function verificaAbreviacao(serie){
+ 	let tamanhoElencoFiltrado = 0;
+ 	let tamanhoElenco
+
+ 	tamanhoElencoFiltrado = serie.elenco.filter(elenco => elenco.includes(".")).length;
+ 	tamanhoElenco = serie.elenco.length;
+
+ 	return tamanhoElenco === tamanhoElencoFiltrado;
+
+ }
+
+
+ function removePontos(string){
+ 	let palavraSemPonto = "";
+
+ 	for(let i = 0 ; i < string.length ; i++){
+ 		
+ 		palavraSemPonto += string[i] === "." ? "" : string[i];  
+ 	}
+ 	return palavraSemPonto;
+ }
+ 
+ 
+ function easterEgg(series){ 	
+ 	
+ 	var mensagen = "";
+ 	var serie = series.filter(verificaAbreviacao);
+
+ 	for(let i =0; i<serie.length; i++){
+ 		
+ 		for(let j =0 ; j < serie[i].elenco.length ; j++){
+
+ 			mensagen += serie[i].elenco[j].split(" ",2).slice(1);
+ 		}
+ 	} 
+
+ 	mensagen = removePontos(mensagen);		
+ 	return mensagen
+ }	
+
+console.log('--------------------- EASTER EGG ------------------------------')
+console.log(easterEgg(series));
