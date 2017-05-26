51de456d2033cfcc70d0d5e4e3ab6b36ba7de2b2
rafaelbarreto <rafael.barreto@cwi.com.br>
2017-05-26 19:40:24 -0300

    feat(Modulo06ExercicioChat)

---
+++ modulo06/aula03/Chat/BackEnd/ChatAPI/ChatAPI/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using ChatAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChatAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+        private static int Id = 1;
+        private static List<Usuario> listaUsuarios = new List<Usuario>();
+
+        public IEnumerable<Usuario> Get(int Id)
+        {
+            return listaUsuarios;
+        }
+
+        public IHttpActionResult Post(string nome)
+        {
+            Usuario usuario = new Usuario();
+            usuario.Nome = nome;
+            listaUsuarios.Add(usuario);
+            usuario.Id = Id++;
+
+            return Ok("usuario cadastrado com sucesso");
+        }
+    }
+}
---
+++ modulo06/aula03/Chat/BackEnd/ChatAPI/ChatAPI/Models/Mensagem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatAPI.Models
+{
+    public class Mensagem
+    {
+        public string Mensagen { get; set; }
+        public Usuario Usuario { get; set; }
+
+    }
+}
