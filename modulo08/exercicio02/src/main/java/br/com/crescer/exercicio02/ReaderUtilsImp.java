9d2af4ced97df142c89d7514910882fb305bac64
rafaelbarreto <rafael.barreto@cwi.com.br>
2017-06-21 22:54:34 -0300

    feat(modulo08Exercicio02): implementar metodo Reader

---
+++ modulo08/exercicio02/src/main/java/br/com/crescer/exercicio02/ReaderUtilsImp.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.exercicio02;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ *
+ * @author usuario
+ */
+public class ReaderUtilsImp implements ReaderUtils {
+
+    @Override
+    public String read(String string) {
+        StringBuilder texto = new StringBuilder();
+        String linhaAtual;
+        BufferedReader buffer = null;
+        FileReader file = null;
+        
+        if(string.contains(".txt")){
+            try{
+                file = new FileReader(string);
+                buffer = new BufferedReader(file);                
+                
+                while((linhaAtual = buffer.readLine()) != null){
+                    texto.append(linhaAtual+"\n");
+                }
+            }catch(Exception e){
+                System.out.println(e);
+            }
+        }
+        return texto.toString();
+    }    
+}
--- modulo08/exercicio02/src/main/java/br/com/crescer/exercicio02/main.java
+++ modulo08/exercicio02/src/main/java/br/com/crescer/exercicio02/main.java
@@ -15,10 +15,12 @@ public class main {
     
     public static void main(String[]args){     
         FileUtisImp file = new FileUtisImp();        
+        ReaderUtilsImp ler = new ReaderUtilsImp();
         System.out.println(file.ls("c:\\windows\\"));//lista pasta
         System.out.println(file.ls("c:\\windows\\win.ini"));//lista pasta
         file.mk("c:\\downloads\\teste");//cria diretorio
         file.mk("c:\\downloads\\teste.txt");//cria arquivo
         file.mv("c:\\downloads\\teste.txt", "c:\\downloads\\teste");
+        System.out.println(ler.read("c:\\downloads\\teste\\teste.txt"));
     }
 }
