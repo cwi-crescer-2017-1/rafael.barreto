9a89b89f2b05b3ad24f0f5d50f1499924c46c555
rafaelbarreto <rafael.barreto@cwi.com.br>
2017-07-04 17:04:48 -0300

    feat(redesocial):implementar ajustes na home page

--- modulo08/RedeSocial/FrontEnd/js/cadastroController.js
+++ modulo08/RedeSocial/FrontEnd/js/cadastroController.js
@@ -1,13 +1,13 @@
 
 
-modulo.controller("CadastroController",function($scope,UsuarioService,toastr){
+modulo.controller("CadastroController",function($scope,$location,UsuarioService,toastr){
 
         $scope.Cadastrar = Cadastrar;        
 
         function Cadastrar(usuario){                     
             UsuarioService.Cadastrar(usuario).then(function(response){
                  toastr.success('Cadastrado com sucesso', 'Sucesso');
+                 $location.path('/login');
             })
         }
-
 })
